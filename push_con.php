335873af5266e4166fba1bd79019afc6ab162e10
Ging Para <nongeng52@gmail.com>
2019-07-16 16:56:37 +0700

    Update push_con.php

--- push_con.php
+++ push_con.php
@@ -2,13 +2,11 @@
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set("Asia/Bangkok");
 $mytime = date("H:i");
-if($mytime >="01:00" && $mytime <= "18:59"){
 	
 		$userId = "ldAxAPwRhs4oZf6cPWEqKqEsqDL8tjg4lrTzWuS0fVR";
                 //   C2048a27bf88cc4a85dcd1b7feb0406e5";
                 $strAccessToken = "8Lg+b3u0aF0TiheDr/QcHGTl8dUs/WyJ3mTWXHOXWzoWLwiE9qafKtnHGkfOZqDT/91i3Vb5zrx9SCDfe1th/Ad6bfbmxgY+RLCpURlW4S/2kLJFZCcINptLYSZUufA6wGDfAduIgzXBC4hQS1SZlgdB04t89/1O/w1cDnyilFU=";
-                $txt = file_get_contents("http://dam.egat.co.th/line_data/auto_bb_sk_line_c.php");
-				//$txt = "ขอบคุณ ";
+                $txt = $txt = "ขอบคุณ ";
                 $txt1 = $txt."\r\n Update ข้อมูล \r\n http://10.242.1.50/sk/sk_plan.php";
                 $strUrl = "https://api.line.me/v2/bot/message/push";
                 $arrHeader = array();
@@ -28,12 +26,7 @@ if($mytime >="01:00" && $mytime <= "18:59"){
                 $result = curl_exec($ch);
                 curl_close ($ch);
                  
-        echo "Send Line BB OK---<br/>";
-        sleep(10);//seconds to wait..
-        header("Location:https://sirikitdam.herokuapp.com/close.htm");
-} else {
-        echo "No Time Send".$mytime;
-         sleep(5);//seconds to wait..
-        header("Location:https://sirikitdam.herokuapp.com/close.htm");
+        echo "Send Line  OK---<br/>";
+
         }
 ?>
